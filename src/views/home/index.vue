ace997b29304cbe0d92144f8038490282edb4f56
ZFRenato <renatopscarlos@gmail.com>
2022-11-16 20:01:40 -0300

    feat: create component contact for home

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,38 @@
 <template>
-  <customHeader/>
+  <customHeader
+    @create-acount="handlerAcountCreat"
+    @login="handlerLogin"
+  />
   <contact/>
   <div class="flex justify-center py-10 bg-brand-gray">
     <p class="font-medium text-center text-gray-800">feedbacker &copy; 2022</p>
   </div>
 </template>
+<script>
+import { onMounted } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
+import customHeader from './customHeader.vue'
+import contact from './contact.vue'
+
+export default {
+  components: { customHeader, contact },
+
+  setup () {
+    const router = useRouter()
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedback' })
+      }
+    })
+
+    function handlerLogin () { }
+    function handlerAcountCreate () { }
+
+    return {
+      handlerLogin,
+      handlerAcountCreate
+    }
+  }
+}
+</script>
